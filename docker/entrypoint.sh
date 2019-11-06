29d46db8de6ba9e95bbd489df0b03f0e88a55138
Damian Czaja <trojan295@gmail.com>
2019-11-06 21:26:10 +0100

    read ip addresses from file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd /
+
 export SERVER_HOST="${SERVER_HOST:-127.0.0.1}"
 export SERVER_PORT=${SERVER_PORT:-10110}
 
@@ -10,7 +12,13 @@ export AUTH_TYPE="${AUTH_TYPE:-jwt}"
 
 export ETCD_ADDRESS="${ETCD_ADDRESS:-http://127.0.0.1:2379}"
 
-cd /
+IP_ADDRESSES_FILE="ip_addresses"
+if [ -f "${IP_ADDRESSES_FILE}" ]; then
+    FILE_IP_ADDRESSES=$(cat ${IP_ADDRESSES_FILE})
+fi
+
+export IP_ADDRESSES="${IP_ADDRESSES:-$FILE_IP_ADDRESSES}"
+
 envsubst < chinchilla.toml.tmpl > chinchilla.toml
 
 exec $@
